138f0264f66aaf9eb493f8c9a8c5c6161cb044ac
Marciom99 <marciomrm99@hotmail.com>
2020-02-19 23:28:07 +0000

    Update test_fasta_to.py

--- Testing/TestScript/test_fasta_to.py
+++ Testing/TestScript/test_fasta_to.py
@@ -4,14 +4,14 @@ import filecmp
 def test_fasta_to_nexus():
     f = fasta_converter("Testing/TestFilesInput/testfasta.fasta", "fasta", "nexus", "no")
 
-    assert filecmp.cmp('Testing/TestFilesInput/testfasta.nex','Testing/TestFilesOutput/testfastaToNex.nex',shallow=False)
+    assert filecmp.cmp('Testing/TestFilesInput/testfasta.nex','Testing/TestFilesOutput/testfastaToNex.nex',shallow=True)
 
 def test_fasta_to_phylip():
     f = fasta_converter("Testing/TestFilesInput/testfasta.fasta", "fasta", "phylip", "no")
 
-    assert filecmp.cmp('Testing/TestFilesInput/testfasta.phy','Testing/TestFilesOutput/testfastaToPhy.phy',shallow=False)
+    assert filecmp.cmp('Testing/TestFilesInput/testfasta.phy','Testing/TestFilesOutput/testfastaToPhy.phy',shallow=True)
 
 def test_fasta_to_aln():
     f = fasta_converter("Testing/TestFilesInput/testfasta.fasta", "fasta", "aln", "no")
 
-    assert filecmp.cmp('Testing/TestFilesInput/testfasta.aln','Testing/TestFilesOutput/testfastaToAln.aln',shallow=False)
+    assert filecmp.cmp('Testing/TestFilesInput/testfasta.aln','Testing/TestFilesOutput/testfastaToAln.aln',shallow=True)
