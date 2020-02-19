fd462d738719623d454abdf2426c76735e38056d
Marciom99 <marciomrm99@hotmail.com>
2020-02-19 23:23:40 +0000

    Update test_fasta_to.py

--- Testing/TestScript/test_fasta_to.py
+++ Testing/TestScript/test_fasta_to.py
@@ -9,9 +9,9 @@ def test_fasta_to_nexus():
 def test_fasta_to_phylip():
     f = fasta_converter("Testing/TestFilesInput/testfasta.fasta", "fasta", "phylip", "no")
 
-    assert filecmp.cmp('testfasta.phy','Testing/TestFilesOutput/testfastaToPhy.phy',shallow=False)
+    assert filecmp.cmp('Testing/TestFilesInput/testfasta.phy','Testing/TestFilesOutput/testfastaToPhy.phy',shallow=False)
 
 def test_fasta_to_aln():
     f = fasta_converter("Testing/TestFilesInput/testfasta.fasta", "fasta", "aln", "no")
 
-    assert filecmp.cmp('testfasta.aln','Testing/TestFilesOutput/testfastaToAln.aln',shallow=False)
+    assert filecmp.cmp('Testing/TestFilesInput/testfasta.aln','Testing/TestFilesOutput/testfastaToAln.aln',shallow=False)
